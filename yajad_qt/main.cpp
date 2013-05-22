c8e712945f3a6403e4db857f347639853324f706
taptap <alexey.e.egorov@gmail.com>
2013-05-22 22:44:24 +0400

    added explicit string encoding

--- yajad_qt/main.cpp
+++ yajad_qt/main.cpp
@@ -1,9 +1,11 @@
 #include "mainwindow.hpp"
+#include <QTextCodec>
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8"));
     MainWindow w;
     w.show();
     
